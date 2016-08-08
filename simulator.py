b9756dbdedcb97a2245be4ba3656ff8530c0b580
karan1149 <karan1149@gmail.com>
2016-08-08 00:59:58 -0400

    Attempts to get first and last dates and symbols.

--- simulator.py
+++ simulator.py
@@ -7,3 +7,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import math
+import sys
+
+df = pd.read_csv(sys.argv[1])
+
+# convert pandas column to non-duplicated lists
+dates = list(set(df[0].tolist())).sort()
+symbols = list(set(df[1].tolist()))
+
+# get first and last dates 
+dt_first = dates[0]
+dt_last = dates[len(dates) - 1] + dt.timedelta(days=1)
