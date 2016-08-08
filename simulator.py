efb4ad6f2b06c32efdf9880df986bcecadd645ce
karan1149 <karan1149@gmail.com>
2016-08-08 00:17:19 -0400

    Imports and simulator file.

---
+++ simulator.py
@@ -0,0 +1,9 @@
+# Using QuantSoftwareToolKit and concommitant libraries
+import QSTK.qstkutil.qsdateutil as du
+import QSTK.qstkutil.tsutil as tsu
+import QSTK.qstkutil.DataAccess as da
+import datetime as dt
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import math
