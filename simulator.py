aa057586302d01e7d3d97a83a8ec16c269313ec6
karan1149 <karan1149@gmail.com>
2016-08-08 01:07:22 -0400

    Accessing data from Yahoo.

--- simulator.py
+++ simulator.py
@@ -15,6 +15,14 @@ df = pd.read_csv(sys.argv[1])
 dates = list(set(df[0].tolist())).sort()
 symbols = list(set(df[1].tolist()))
 
-# get first and last dates 
-dt_first = dates[0]
-dt_last = dates[len(dates) - 1] + dt.timedelta(days=1)
+# get first and last dates
+dt_start = dates[0]
+dt_end = dates[len(dates) - 1] + dt.timedelta(days=1)
+
+dt_timeofday = dt.timedelta(hours=16)
+ldt_timestamps = du.getNYSEdays(dt_start, dt_end, dt_timeofday)
+
+c_dataobj = da.DataAccess('Yahoo')
+ls_keys = ['close']
+key_data = c_dataobj.get_data(ldt_timestamps, symbols, ls_keys)
+data = key_data[0]
