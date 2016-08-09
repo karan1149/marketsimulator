9c623a4fc31d3e609f2659b5ceed9e84ad095e28
karan1149 <karan1149@gmail.com>
2016-08-09 15:25:15 -0400

    Added cum sum for holdings and cash dataframe.

--- simulator.py
+++ simulator.py
@@ -9,11 +9,11 @@ import numpy as np
 import math
 import sys
 
-df = pd.read_csv(sys.argv[1])
+orders = pd.read_csv(sys.argv[1])
 
 # convert pandas column to non-duplicated lists
-dates = list(set(df[0].tolist())).sort()
-symbols = list(set(df[1].tolist()))
+dates = list(set(orders[0].tolist())).sort()
+symbols = list(set(orders[1].tolist()))
 
 # get first and last dates
 dt_start = dates[0]
@@ -26,3 +26,26 @@ c_dataobj = da.DataAccess('Yahoo')
 ls_keys = ['close']
 key_data = c_dataobj.get_data(ldt_timestamps, symbols, ls_keys)
 data = key_data[0]
+
+full_dates = [dt_start + datetime.timedelta(days=x) for x in range(0, (dt_end-dt_start).days)]
+
+# create empty dataframe for trade matrix
+a = np.zeros(shape=(len(full_dates), len(symbols)))
+trades = pd.DataFrame(a, columns=symbols)
+trades.set_index(full_dates)
+
+casha = np.zeros(shape=(len(full_dates), 1))
+cash = pd.DataFrame(casha)
+cash.set_index(full_dates)
+
+# iterate through trades to create trade matrix
+for index, row in orders.iterrows():
+    amount = row[3]
+    if row[2] == "SELL":
+        amount = amount * -1
+    trades[row[0], row[1]] = trades[row[0], row[1]] + amount
+    cash[row[0], 0] = cash[row[0], 0] - amount * data[row[0], row[1]]
+
+holdings = trades.cumsum(axis = 0)
+
+total_stocks = holdings.dot(data)
